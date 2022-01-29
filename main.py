9072b52233ad30f83494783c139301f5b49d37d3
josh-campbell6 <joshmcccampbell6@gmail.com>
2022-01-29 11:24:29 -0500

    Merge commit '6d9a1d57ab714bf8fc6319165714b87950af510c' of https://github.com/mowglu/MIAE-Python
    # Conflicts:
    #	LICENSE

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def main_wrapper():
     print(f"This is the start of our Python Project. This function's name is {main_wrapper.__name__}")
 
-    # Code here
 
-    #git
+
+
+
+
+
+
+
     print("This is the end of our Python Project")
 
 
