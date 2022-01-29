48434bb1558042520ecad3222cefc3b130cd434a
josh-campbell6 <joshmcccampbell6@gmail.com>
2022-01-29 11:04:36 -0500

    This is my first Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main_wrapper():
+    print(f"This is the start of our Python Project. This function's name is {main_wrapper.__name__}")
+
+    # Code here
+
+    #git
+    print("This is the end of our Python Project")
+
+
+if __name__ == "__main__":
+    main_wrapper()
